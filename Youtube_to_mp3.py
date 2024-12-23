b240b3cfc4349108ceef890392780f380d630102
Alexander McKinley <alexanderfromaustralia@gmail.com>
2024-12-23 16:00:21 +0900

    Add files via upload

---
+++ Youtube_to_mp3.py
@@ -0,0 +1,35 @@
+import yt_dlp
+import os
+
+run = True
+URL = ""
+name = ""
+
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+ffmpeg_path = os.path.join(os.path.expanduser("~"), "ffmpeg-2024-12-19-git-494c961379-essentials_build/bin")
+
+while run:
+    print("▶️YouTubeダウンローダー🎧")
+    URL = input("URLを入力するか、Qで終了: ")
+    if URL.lower() == "q":
+        run = False
+        break
+    name = input("ファイル名（拡張子なし）を入力: ")
+    opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': os.path.join(desktop_path, f'{name}.mp3'),
+        'postprocessors': [
+            {
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }
+        ],
+        'ffmpeg_location': ffmpeg_path,
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(opts) as ydl:
+            ydl.download([URL])
+    except Exception as e:
+        print(f"エラー: {e}")
